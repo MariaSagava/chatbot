2a439bf5f32f333acf4ea718f4f7f8eed0bacf28
MariaSagava <maria.ed.iss.m.s@gmail.com>
2025-06-26 08:28:50 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 
 const API_KEY = "AIzaSyCq6BRjupNXHgvC0OK5i_hYj-_3rQ796XQ"; 
-OPENWEATHER_API_KEY="eed582bff717fa84c6e84f8f8738de61";
+// MONGO_URI e OPENWEATHER_API_KEY foram movidos para as variáveis de ambiente do servidor por segurança.
 
-
-console.log("Configurações carregadas com sucesso!");
+console.log("Configurações de cliente carregadas com sucesso!");
